f0317d7e6c19fe81eeb230f117a1b2e8425fba61
Eli Tamosauskas <tamosauskas09@gmail.com>
2025-01-25 16:22:19 +0200

    clean up

---
+++ README.md
@@ -0,0 +1,1 @@
+Just run `npm run test` to run with vitest or `npm run dev` to run in browser env.
--- src/main.test.ts
+++ src/main.test.ts
@@ -1,6 +1,12 @@
-import { sum } from "./main";
+import { sum, subtract } from "./main";
 import { expect, test } from "vitest";
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(4);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(3, 2)).toBe(5);
+});
+
+test("subtracts 1 + 2 to equal 3", () => {
+  expect(subtract(3, 1)).toBe(2);
+  expect(subtract(6, 1)).toBe(5);
 });
